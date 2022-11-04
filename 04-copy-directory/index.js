6bcab32558fcfb32c4ff8ca67f50ce46ed977551
nov1cov <novicov-andrey@yandex.ru>
2022-11-04 10:07:43 +0300

    init: index.js in 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,41 @@
 
+const fs = require('fs');
+const path =require('path');
+
+function init() {
+  fs.mkdir(path.join(__dirname, 'files-copy'),
+  { recursive: true },
+    err => {
+      if(err) throw err;
+      console.log('Папка была создана'); 
+    }     
+  );
+}
+
+function getFile() {
+
+  fs.readdir(path.join(__dirname, 'files'),  
+    (err, files) => {
+      if(err) throw err;
+      files.forEach(file => {      
+        copyDir(file);
+      }); 
+    }
+  );
+}
+
+function copyDir(files) {
+  fs.copyFile(path.join(__dirname, 'files', files), path.join(__dirname, 'files-copy', files), 
+    (err) => {
+      if (err) {
+      console.log(err);
+    }
+  });
+}
+
+fs.access('files-copy', 
+  fs.constants.F_OK, (err) => {
+    if(err) init();
+    getFile();
+  }
+);
